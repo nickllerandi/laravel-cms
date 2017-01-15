237b88e8014e74187c5fe4be4afd2edc714b174e
Nicholas Llerandi <nllerandi@Nicholass-MacBook-Air.local>
2017-01-15 18:41:00 -0500

    changing copy

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,29 +3,29 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <p class="quote">Laravel Blog</p>
+            <p class="quote">Blogging CMS using Laravel</p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 class="post-title">Learning Laravel</h1>
-            <p>This blog post will get you right on track with Laravel!</p>
+            <h1 class="post-title">Tony the tiger gets hit by a train</h1>
+            <p>oh no that's too bad</p>
             <p><a href="{{ route('blog.post', ['id' => 1]) }}">Read more...</a></p>
         </div>
     </div>
     <hr>
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 class="post-title">The next Steps</h1>
-            <p>Understanding the Basics is great, but you need to be able to make the next steps.</p>
+            <h1 class="post-title">Count chocula is actually a lunatic</h1>
+            <p>Don't eat this man's cereal</p>
             <p><a href="{{ route('blog.post', ['id' => 2]) }}">Read more...</a></p>
         </div>
     </div>
     <hr>
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 class="post-title">Laravel 5.3</h1>
-            <p>Though announced as a "minor release", Laravel 5.3 ships with somer very interesting additions and features.</p>
+            <h1 class="post-title">Fred flintstone's license gets revoked</h1>
+            <p>...for hitting too many dinosaurs</p>
             <p><a href="{{ route('blog.post', ['id' => 3]) }}">Read more...</a></p>
         </div>
     </div>
